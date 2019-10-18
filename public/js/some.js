251cc5d922a2a0cf2e5f664d3b72bd73dade64b6
Dezii67 <Dezii67@users.noreply.github.com>
2019-10-18 20:28:43 +0600

    Login and registration page created

--- public/js/some.js
+++ public/js/some.js
@@ -1,1 +1,11 @@
-alert("it works")
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
